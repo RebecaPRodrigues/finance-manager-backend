c5cd36b87cbe75bff26156234da39254fb7faa43
User <User@DESKTOP-BG6JCBI>
2025-03-29 16:19:41 -0300

    fix token service

--- src/main/java/com/exemplo/webappfinancemanager/config/TokenService.java
+++ src/main/java/com/exemplo/webappfinancemanager/config/TokenService.java
@@ -11,6 +11,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.exemplo.webappfinancemanager.entity.User;
 
 @Service
@@ -21,14 +22,14 @@ public class TokenService {
     public String generateToken(User user){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
+            return JWT.create()
                     .withIssuer("auth-api")
                     .withSubject(user.getLogin())
+                    .withClaim("role", "ROLE_" + user.getRole()) 
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
-            return token;
         } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while generating token", exception);
+            throw new RuntimeException("Erro ao gerar token", exception);
         }
     }
 
@@ -41,8 +42,18 @@ public class TokenService {
                     .verify(token)
                     .getSubject();
         } catch (JWTVerificationException exception){
-        	 throw new RuntimeException("Token inválido", exception);
+            throw new RuntimeException("Token inválido", exception);
         }
+    }
+
+    public String extractRole(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        DecodedJWT decodedJWT = JWT.require(algorithm)
+                .withIssuer("auth-api")
+                .build()
+                .verify(token);
+
+        return decodedJWT.getClaim("role").asString();
     }
 
     private Instant genExpirationDate(){
