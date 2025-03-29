83629871e306d87d4bb3c9c8e833416c8840075e
User <User@DESKTOP-BG6JCBI>
2025-03-29 15:58:50 -0300

    ajustando seguranca transaction

--- src/main/java/com/exemplo/webappfinancemanager/controller/TransactionController.java
+++ src/main/java/com/exemplo/webappfinancemanager/controller/TransactionController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class TransactionController {
     }
 
     @PostMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> createTransaction(@RequestBody RegisterTransactionDTO transaction) {
     	ViewTransactionDTO savedTransaction = service.save(transaction);
         return ResponseEntity.ok(savedTransaction);
